a2eb4a5b3e5fddf1992a210345312e1544224ab7
NeKit <nekit1000@gmail.com>
2018-05-08 23:20:27 +0300

    Change glFinish() back to glFlush() in glamor_block_handler
    Partially revert rockchip-linux/xserver@24582ff to improve performance.
    
    Our Android Mali driver blob hasn't been noticed yet to suffer from
    the same issue that caused the original change in the first place.

--- src/glamor.c
+++ src/glamor.c
@@ -1100,5 +1100,5 @@ glamor_finish(ScreenPtr screen)
     glamor_screen_private *glamor_priv = glamor_get_screen_private(screen);
 
     glamor_make_current(glamor_priv);
-    glFinish();
+    glFlush();
 }
